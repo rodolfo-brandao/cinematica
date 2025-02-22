b8a7ca2cdc0051e3817a14baad137ad87a62365c
Rodolfo <rodolfobrandao1995@gmail.com>
2025-02-21 22:46:50 -0300

    refactor: improve OpenAPI documentation

--- src/MovieLibrary.Presentation/Controllers/UsersController.cs
+++ src/MovieLibrary.Presentation/Controllers/UsersController.cs
@@ -21,7 +21,6 @@ public class UsersController(IMediator mediator) : ApiResultController
     [AllowAnonymous]
     [HttpPost("new", Name = "create-user")]
     [ProducesResponseType(statusCode: StatusCodes.Status201Created, type: typeof(CreatedUserResponse))]
-    [ProducesResponseType(statusCode: StatusCodes.Status400BadRequest, type: typeof(string))]
     public async Task<IActionResult> CreateUserAsync([FromBody] CreateUserCommand command,
         CancellationToken cancellationToken)
     {
@@ -29,16 +28,15 @@ public class UsersController(IMediator mediator) : ApiResultController
     }
 
     /// <summary>
-    /// Authorizes an existing user, according to its role, by issuing a JSON Web Token (JWT).
+    /// Authorizes an existing user by issuing a JSON Web Token (JWT).
     /// </summary>
     /// <param name="command">The payload containing all data needed to validate a user and issue a JSON Web Token (JWT).</param>
     /// <param name="cancellationToken">A token that propagates notification that this request should be canceled.</param>
-    /// <response code="200">Returns the newly issued JSON Web Token (JWT).</response>
+    /// <response code="200">Returns the newly issued JSON Web Token (JWT) along with some essential information.</response>
     /// <response code="400">There was an error when trying to validate the request payload.</response>
     [AllowAnonymous]
     [HttpPost("token", Name = "authorize-user")]
     [ProducesResponseType(statusCode: StatusCodes.Status200OK, type: typeof(AuthorizedUserResponse))]
-    [ProducesResponseType(statusCode: StatusCodes.Status400BadRequest, type: typeof(string))]
     public async Task<IActionResult> IssueJsonWebTokenAsync([FromBody] AuthorizeUserCommand command,
         CancellationToken cancellationToken)
     {
