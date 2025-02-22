e37529fede36f7d4b13f0dd1f7a58fb1f25a97e0
Rodolfo <rodolfobrandao1995@gmail.com>
2025-02-21 22:15:10 -0300

    refactor: change constant types from 'ushort' to 'byte' / move validation on username length to a separate rule

--- src/MovieLibrary.Application/Commands/Users/CreateUser/CreateUserCommandValidator.cs
+++ src/MovieLibrary.Application/Commands/Users/CreateUser/CreateUserCommandValidator.cs
@@ -5,22 +5,23 @@ namespace MovieLibrary.Application.Commands.Users.CreateUser;
 
 public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
 {
-    private const ushort UsernameMinLength = 3;
-    private const ushort UsernameMaxLength = 50;
-    private const ushort EmailMaxLength = 255;
-    private const ushort PasswordMinLength = 8;
+    private const byte UsernameMinLength = 3;
+    private const byte UsernameMaxLength = 50;
+    private const byte EmailMaxLength = 255;
+    private const byte PasswordMinLength = 8;
 
     public CreateUserCommandValidator(IUserRepository userRepository)
     {
         RuleFor(command => command.Username)
             .NotEmpty()
             .WithMessage("The username cannot be empty, null or whitespace.")
-            .MinimumLength(minimumLength: UsernameMinLength)
-            .WithMessage($"The username must have at least {UsernameMinLength} characters.")
-            .MaximumLength(maximumLength: UsernameMaxLength)
-            .WithMessage($"The username must not exceed {UsernameMaxLength} characters.")
-            .MustAsync(async (username, _) => !await userRepository.ExistsAsync(user => user.Username.Equals(username) && !user.IsDisabled))
+            .MustAsync(async (username, _) =>
+                !await userRepository.ExistsAsync(user => user.Username.Equals(username) && !user.IsDisabled))
             .WithMessage(command => $"User with username '{command.Username}' already exists.");
+
+        RuleFor(command => command.Username.Length)
+            .InclusiveBetween(from: UsernameMinLength, to: UsernameMaxLength)
+            .WithMessage($"The username must be between {UsernameMinLength} and {UsernameMaxLength} characters.");
 
         RuleFor(command => command.Email)
             .NotEmpty()
@@ -29,7 +30,8 @@ public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
             .WithMessage($"The e-mail must not exceed {EmailMaxLength} characters.")
             .Must(email => MailAddress.TryCreate(email, out var _))
             .WithMessage("The e-mail address is not valid.")
-            .MustAsync(async (email, _) => !await userRepository.ExistsAsync(user => user.Email.Equals(email) && !user.IsDisabled))
+            .MustAsync(async (email, _) =>
+                !await userRepository.ExistsAsync(user => user.Email.Equals(email) && !user.IsDisabled))
             .WithMessage(command => $"User with e-mail '{command.Email}' already exists.");
 
         RuleFor(command => command.Password)
