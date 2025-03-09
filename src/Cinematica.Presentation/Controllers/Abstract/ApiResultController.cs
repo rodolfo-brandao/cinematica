fb845a7169731f9dba9e8b0420d6e2aac474dd41
Rodolfo <rodolfobrandao1995@gmail.com>
2025-03-09 15:39:24 -0300

    refactor: add parameter for action name / add error message in 404 responses

--- src/Cinematica.Presentation/Controllers/Abstract/ApiResultController.cs
+++ src/Cinematica.Presentation/Controllers/Abstract/ApiResultController.cs
@@ -12,15 +12,17 @@ public abstract class ApiResultController : ControllerBase
     /// Builds the proper status code object based on the <see cref="ApiResult{TResponse}"/>.
     /// </summary>
     /// <typeparam name="TResponse">The type of the response.</typeparam>
-    protected IActionResult BuildStatusCodeObject<TResponse>(ApiResult<TResponse> apiResult)
+    /// <param name="apiResult">The respective API result object containing the success or failure response.</param>
+    /// <param name="actionName">The name of the action to use for generating the redirect URL for when the response status code is '201 Created'.</param>
+    protected IActionResult BuildStatusCodeObject<TResponse>(ApiResult<TResponse> apiResult, string actionName = null)
     {
         return apiResult.StatusCode switch
         {
             StatusCodes.Status200OK => Ok(apiResult.Response),
-            StatusCodes.Status201Created => CreatedAtAction(actionName: default, apiResult.Response),
+            StatusCodes.Status201Created => CreatedAtAction(actionName: actionName, apiResult.Response),
             StatusCodes.Status204NoContent => NoContent(),
             StatusCodes.Status400BadRequest => BadRequest(apiResult.ErrorMessage),
-            StatusCodes.Status404NotFound => NotFound(),
+            StatusCodes.Status404NotFound => NotFound(value: apiResult.ErrorMessage),
             _ => Problem(statusCode: StatusCodes.Status500InternalServerError, detail: apiResult.ErrorMessage),
         };
     }
