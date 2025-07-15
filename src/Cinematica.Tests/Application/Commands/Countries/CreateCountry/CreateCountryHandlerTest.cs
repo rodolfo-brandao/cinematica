3373e8ab515f4d44b5ae4c64d5031c72788e13cf
Rodolfo <rodolfobrandao1995@gmail.com>
2025-07-14 22:12:27 -0300

    WIP

---
+++ src/Cinematica.Tests/Application/Commands/Countries/CreateCountry/CreateCountryHandlerTest.cs
@@ -0,0 +1,7 @@
+﻿namespace Cinematica.Tests.Application.Commands.Countries.CreateCountry;
+
+[Trait(name: "Handler(command)", value: "CreateCountry")]
+public class CreateCountryHandlerTest
+{
+    // TODO
+}
