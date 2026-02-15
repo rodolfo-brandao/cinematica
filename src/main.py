562648ed1da079f4c64f391c7bc384de4cdf2bba
rodolfo-brandao <rodolfobrandao1995@gmail.com>
2026-02-15 16:45:51 -0300

    feat: add project entrypoint

---
+++ src/main.py
@@ -0,0 +1,14 @@
+"""
+The application entrypoint.
+
+From the root folder, run `python3 -m src.main`
+"""
+
+
+from pathlib import Path
+from src.pipeline.step1_filter_movies import run
+
+
+if __name__ == "__main__":
+    imdb_title_basics_file_path = Path("datasets/title.basics.tsv.gz")
+    run(imdb_title_basics_file_path)
