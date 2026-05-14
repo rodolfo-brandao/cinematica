f6be559f3f20a9a912fdf87c6d0802d40bbdc59f
rodolfo-brandao <rodolfobrandao1995@gmail.com>
2026-05-14 19:39:43 -0300

    chore(code-style): make const into variable

--- src/imdb/loader.py
+++ src/imdb/loader.py
@@ -14,12 +14,12 @@ def stream_title_basics(file_path: Path) -> Iterator[Dict[str, str]]:
     :rtype: Iterator[Dict[str, str]]
     """
 
-    TAB_CHAR = "\t"
-    UTF8_BOM = "\ufeff"  # Byte Order Mark
+    tab_char = "\t"
+    utf8_bom = "\ufeff"  # Byte Order Mark
 
     with gzip.open(filename=file_path, mode="rt", encoding="utf-8") as file:
-        headers = [h.strip().lstrip(UTF8_BOM) for h in file.readline().split(TAB_CHAR)]
+        headers = [h.strip().lstrip(utf8_bom) for h in file.readline().split(tab_char)]
 
         for line in file:
-            values = [v.strip() for v in line.split(TAB_CHAR)]
+            values = [v.strip() for v in line.split(tab_char)]
             yield dict(zip(headers, values))
