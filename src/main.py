8033863f0ba70178b9dd1291065eef7e02279196
rodolfo-brandao <rodolfobrandao1995@gmail.com>
2026-05-14 19:40:13 -0300

    refactor(wip): simplify main file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,2 @@
-from pathlib import Path
-from src.pipeline.step1_filter_movies import run
-
-
 if __name__ == "__main__":
-    imdb_title_basics_file_path = Path("datasets/title.basics.tsv.gz")
-    run(imdb_title_basics_file_path)
+    print("hello world")
